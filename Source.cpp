7aa6151ce29eec57f69f1fe2ae6cb0a86c3c7c4e
Shmouel Hai Illouz <57444008+ShmouelHai@users.noreply.github.com>
2020-12-06 23:56:32 +0200

    Update Source.cpp

--- Source.cpp
+++ Source.cpp
@@ -2,23 +2,28 @@
 #include <string>
 using namespace std;
 
+//function to check if the number is palindrome
 bool isPalindrome(int num) {
+	//temp for help, and rev for reverse
 	int temp = num, rev = 0;
-
+	
+	//loop to create the reverse number
 	while(temp > 0)
 	{
-		rev *= 10;
-		rev += temp % 10; 
-		temp /= 10; 
+		rev *= 10; //multiply by 10 
+		rev += temp % 10; //and add the last number of the temp
+		temp /= 10; //remove the last digit of the number
 	}
-	return num == rev;
+	return num == rev; //return the equality if yes it's 1 else it returns 0
 }
 
-
+//main fonction
 int main(){
 	int n;
+	//enter the number to check
 	cin >> n;
 
+	//call the function and collect the result
 	if (isPalindrome(n)) cout << n << " is palindrome" << endl;
 	else cout << n << " is NOT palindrome" << endl;
 	return 0;
